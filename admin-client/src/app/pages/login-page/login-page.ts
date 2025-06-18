e96cd220f09e4bbc8dae7f66e31c44d0f630cc28
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-18 14:44:57 +0300

    Add login page component

---
+++ admin-client/src/app/pages/login-page/login-page.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Auth } from '../../services/auth';
+
+@Component({
+  selector: 'app-login-page',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login-page.html',
+  styleUrl: './login-page.css'
+})
+export class LoginPage {
+ 
+  loginForm: FormGroup;
+  
+  constructor(private authService: Auth) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(4)])
+    });
+  }
+  
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (response) => {
+          console.log('Login successful', response);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+        }
+      });
+    } else {
+      console.warn('Form is invalid');
+    }
+  }
+}
