a66fcf86ebdb21276f3cc7720a7885d86e78d7c0
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-18 14:44:05 +0300

    Add Auth Service

---
+++ admin-client/src/app/services/auth.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  private apiUrl = `${environment.apiUrl}/auth`;
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+
+  constructor(private http: HttpClient) {
+    const storedAuth = this.getAuthStatusFromStorage();
+    this.isAuthenticatedSubject.next(storedAuth);
+   }
+
+  login(email: string, password: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/login`, { email, password }, { withCredentials: true }).pipe(
+      tap(() => {
+        this.setAuthenticated(true);
+      })
+    );
+  }
+
+  logout() {
+    return this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).pipe(
+      tap(() => {
+        this.setAuthenticated(false)
+      })
+    );
+  }
+
+  refreshToken() {
+    return this.http.post(`${this.apiUrl}/refresh-token`, {}, { withCredentials: true });
+  }
+
+  private getAuthStatusFromStorage(): boolean {
+    return localStorage.getItem('isAuthenticated') === 'true';
+  }
+
+  private setAuthenticated(isAuth: boolean): void {
+    localStorage.setItem('isAuthenticated', isAuth.toString());
+    this.isAuthenticatedSubject.next(isAuth);
+  }
+}
