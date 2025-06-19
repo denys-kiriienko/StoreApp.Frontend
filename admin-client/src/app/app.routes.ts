caf5a166f88bc83f3395d35e89b1ac775198794e
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-19 13:17:29 +0300

    Add product form page with routes

--- admin-client/src/app/app.routes.ts
+++ admin-client/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
 import { ProductListPage } from './pages/product-list-page/product-list-page';
+import { ProductFormPage } from './pages/product-form-page/product-form-page';
 
 export const routes: Routes = [
     { path: 'login', component: LoginPage },
     { path: 'products', component: ProductListPage },
+    { path: 'products/new', component: ProductFormPage },
+    { path: 'products/edit/:id', component: ProductFormPage }
 ];
---
+++ admin-client/src/app/pages/product-form-page/product-form-page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-product-form-page',
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './product-form-page.html',
+  styleUrl: './product-form-page.css'
+})
+export class ProductFormPage implements OnInit {
+
+  productForm!: FormGroup;
+  isEditMode = false;
+  productId?: number;
+
+  constructor(
+    private productService: ProductService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      description: [''],
+      price: [0, [Validators.required, Validators.min(0.01)]],
+      imageUrl: ['']
+    });
+
+    this.route.paramMap.subscribe(params => {
+      const idParam = params.get('id');
+      if (idParam) {
+        this.isEditMode = true;
+        this.productId = +idParam;
+        this.loadProduct(this.productId);
+      }
+    });
+  }
+
+  loadProduct(id: number): void {
+    this.productService.getProductById(id).subscribe(product => {
+      this.productForm.patchValue(product);
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.invalid) return;
+
+    const product: Product = this.productForm.value;
+
+    if (this.isEditMode && this.productId != null) {
+      this.productService.updateProductById(this.productId, product).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    } else {
+      this.productService.addProduct(product).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+}
