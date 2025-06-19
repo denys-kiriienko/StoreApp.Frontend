9b1810aab58e00b0521ce4c2f1618dc2cff0a7a6
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-19 11:34:16 +0300

    Add Product Service

---
+++ admin-client/src/app/services/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private apiUrl = `${environment.apiUrl}/product`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.apiUrl}`, { withCredentials: true });
+  }
+}
