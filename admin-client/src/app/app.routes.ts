0a125d57879909350a8dd12a6dbfbee5a3222c3d
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-19 11:35:38 +0300

    Add route for ProductListPage

--- admin-client/src/app/app.routes.ts
+++ admin-client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from './pages/login-page/login-page';
+import { ProductListPage } from './pages/product-list-page/product-list-page';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginPage }
+    { path: 'login', component: LoginPage },
+    { path: 'products', component: ProductListPage },
 ];
