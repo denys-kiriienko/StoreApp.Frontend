6086c811aeaf805782e8a1bc64d37c3bb2d889c9
denys-kiriienko <denys-kiriienko@vertexcode.dev>
2025-06-24 12:42:28 +0300

    Add sidebar component to app layout

--- admin-client/src/app/app.ts
+++ admin-client/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'admin-client';
+  
+  sidebarIsOpen = false;
+
+  onSidebarToggle(isOpen: boolean) {
+    this.sidebarIsOpen = isOpen;
+  }
 }
